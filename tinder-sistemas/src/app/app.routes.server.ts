6378a1a7e04c3579a01eba6c123a53cf86efae92
LeoMotoMoto200 <lbacac@unsa.edu.pe>
2025-07-03 19:06:40 -0500

    Update

--- tinder-sistemas/src/app/app.routes.server.ts
+++ tinder-sistemas/src/app/app.routes.server.ts
@@ -1,22 +1,8 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
-// Importamos los componentes que creamos
-import { Home } from './pages/home/home';
-import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
-import { Chat } from './pages/chat/chat';
-
-const routes: Routes = [
-  { path: '', component: Home }, // La ruta raíz muestra la landing page
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
-  { path: 'chat', component: Chat },
-  { path: '**', redirectTo: '', pathMatch: 'full' } // Si no encuentra la ruta, redirige al inicio
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
